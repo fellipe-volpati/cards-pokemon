f163523caf836b13606e4ecd2d18e60e4d215270
Fellipe volpati <110109171+fellipe-volpati@users.noreply.github.com>
2022-07-30 22:18:16 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,54 @@
+
+const btnAvancar = document.getElementById('btn-avancar');
+const btnVoltar = document.getElementById('btn-voltar');
+let cartaoAtual = 0;
+
+function esconderCartaoSelecionado() {
+    const cartaoSelecionado = document.querySelector('.selecionado');
+   cartaoSelecionado.classList.remove('selecionado');}
+
+   function mostrarCartao(indiceCartao) {
+    cartoes[indiceCartao].classList.add('selecionado')
+    }
+    
+
+
+
+const cartoes = document.querySelectorAll('.cartao')
+
+
+
+btnAvancar.addEventListener('click', function (){
+   
+    if(cartaoAtual===cartoes.length - 1) return
+
+    esconderCartaoSelecionado();
+   
+   
+    cartaoAtual++;
+   
+   mostrarCartao(cartaoAtual);
+
+  
+  
+   
+
+})
+
+
+
+
+
+ btnVoltar.addEventListener('click', function (){
+   
+    if (cartaoAtual === 0) return;
+    
+    
+    
+   esconderCartaoSelecionado();
+   
+   cartaoAtual--;
+   mostrarCartao(cartaoAtual);
+
+
+});
